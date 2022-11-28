0103b73bfb84a4c19751ff660b076a1a8a2e262d
Ryan Webber <74212280+Rdmwebber@users.noreply.github.com>
2022-11-28 16:23:40 -0500

    wrote function to return svg of streaming service

--- src/components/MediaItem.js
+++ src/components/MediaItem.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Card from "./UI/Card";
 import classes from "./MediaItem.module.css";
+import netflixLogo from "../assets/Netflix.svg";
+import primeLogo from "../assets/Prime.svg";
+import appleTvLogo from "../assets/AppleTV.svg";
+import craveLogo from "../assets/Crave.svg";
+import disneyLogo from "../assets/Disney.svg";
 
 function MediaItem(props) {
-  if (props.mediaObj.streamingInfo.hasOwnProperty("disney")) {
-    console.log(` ${props.mediaObj.title} IS ON DISNEY`);
-  }
+  const checkStreamingService = () => {
+    if (props.mediaObj.streamingInfo.hasOwnProperty("disney")) {
+      return disneyLogo;
+    } else if (props.mediaObj.streamingInfo.hasOwnProperty("netflix")) {
+      return netflixLogo;
+    } else if (props.mediaObj.streamingInfo.hasOwnProperty("crave")) {
+      return craveLogo;
+    } else if (props.mediaObj.streamingInfo.hasOwnProperty("apple")) {
+      return appleTvLogo;
+    } else if (props.mediaObj.streamingInfo.hasOwnProperty("prime")) {
+      return primeLogo;
+    }
+  };
+
+  const streamingService = checkStreamingService();
+
   return (
     <li className={classes.listItems}>
       <Card>
@@ -17,6 +35,7 @@ function MediaItem(props) {
             <h2>{props.mediaObj.title}</h2>
             <h3>{props.mediaObj.year}</h3>
             <h3>Watch It On</h3>
+            <img src={streamingService} alt="Streaming Service" />
           </div>
         </div>
       </Card>
