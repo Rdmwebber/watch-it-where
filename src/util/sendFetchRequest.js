4a46121b89e3e16c33399dc564517e93ede90dfd
Ryan Webber <74212280+Rdmwebber@users.noreply.github.com>
2022-12-27 11:44:27 -0500

    new branch

--- src/util/sendFetchRequest.js
+++ src/util/sendFetchRequest.js
@@ -1,15 +1,15 @@
 import apiOptions from "./api";
 
-const getResults = (title, ctx) => {
+const getResults = (title, ctx, mediaType) => {
   let page = 1;
   let totalPages;
   let searchResults = [];
-
+  //I will be rewriting this whole function
   const sendHttpRequest = async () => {
     ctx.setIsLoading(true);
-    if (page === 1 || page <= totalPages || page < 10) {
+    if ((page === 1 || page <= totalPages) && page < 10) {
       const response = await fetch(
-        `https://streaming-availability.p.rapidapi.com/search/ultra?country=ca&services=netflix%2Cprime%2Cdisney%2Capple%2Ccrave&type=movie&order_by=imdb_vote_count&page=${page}&desc=true&language=en&keyword=${title}&output_language=en`,
+        `https://streaming-availability.p.rapidapi.com/search/ultra?country=ca&services=netflix%2Cprime%2Cdisney%2Capple%2Ccrave&type=${mediaType}&order_by=imdb_vote_count&page=${page}&desc=true&language=en&keyword=${title}&output_language=en`,
         apiOptions
       );
 
@@ -38,7 +38,7 @@ const getResults = (title, ctx) => {
   try {
     sendHttpRequest();
   } catch (err) {
-    console.log(err);
+    console.log("ERROR", err);
   }
 
   console.log(searchResults);
